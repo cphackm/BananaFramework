e19d6d76b040f00ffcfe262761cbdba986095be9
linkman2004 <linkman2004@gmail.com>
2014-12-09 20:51:06 -0600

    Added the Math helper and some basic math functions

---
+++ BananaFramework/Helpers/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BananaFramework.Helpers
+{
+	public class Math
+	{
+		public static float Lerp(float A, float B, float X)
+		{
+			return A + (B - A) * X;
+		}
+
+		public static float Qerp(float A, float B, float C, float X)
+		{
+			return Lerp(Lerp(A, B, X), Lerp(B, C, X), X);
+		}
+
+		public static float Clamp(float Min, float Max, float X)
+		{
+			return X > Max ? Max : (X < Min ? Min : X);
+		}
+	}
+}
